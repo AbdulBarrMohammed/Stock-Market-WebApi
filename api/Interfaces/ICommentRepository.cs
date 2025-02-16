76d693423e406eb20e82607c548f905ad0457991
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-02-15 21:21:14 -0500

    comment delete

--- api/Interfaces/ICommentRepository.cs
+++ api/Interfaces/ICommentRepository.cs
@@ -15,6 +15,8 @@ namespace api.Interfaces
         Task<Comment> CreateAsync(Comment commentModel);
 
         Task<Comment?> UpdateAsync(int id, Comment commentModel);
-        
+
+        Task<Comment?> DeleteAsync(int id);
+
     }
 }
--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7976e575d0f5b7ffa0a33a7436fdb64d7362ab0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01c40665da9e70f579e9afb0dfa42307570eb8df")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
