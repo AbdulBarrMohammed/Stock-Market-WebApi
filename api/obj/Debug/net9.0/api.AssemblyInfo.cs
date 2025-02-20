20e4de8cc0228b2f3c30061e83461e3c84c96a24
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-02-20 12:19:29 -0500

    authorize

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a43d77cac8dc523474635bac4af7d7b5e5402bd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daedf55f8685b5b791d811c41048b987fe9dc62c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
