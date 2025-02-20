b1a16a42c2378b6df23a386e755e23a8fa7e8059
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-02-20 11:11:33 -0500

    new user dto

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b2ba34fd2af708ed1cbfa058c78694ad599c01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0dbc63041a86ae55ac02b275eb79c11f6388453")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
