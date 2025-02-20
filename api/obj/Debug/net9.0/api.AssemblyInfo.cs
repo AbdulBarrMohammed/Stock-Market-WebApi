c602060b3af253198132af66fa0d2dbaa142110e
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-02-20 11:05:20 -0500

    token serivce

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4c4f264a5e4200bb7865fec953651d290ef7967")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b2ba34fd2af708ed1cbfa058c78694ad599c01")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
